259faa90241d6e0f953146b649a72f0af6bd561b
Erik Siefken <git@posporus.de>
2022-05-11 09:36:07 +0200

    init

---
+++ deps.ts
@@ -0,0 +1,12 @@
+export {
+    Router,
+    Application
+} from 'https://deno.land/x/oak@v10.5.1/mod.ts'
+export {
+    h,
+    render,
+    renderSSR,
+    Helmet,
+    Component
+    //Router as NRouter,
+ } from 'https://deno.land/x/nano_jsx@v0.0.30/mod.ts'
---
+++ server.tsx
@@ -0,0 +1,43 @@
+/** @jsx h */
+import {
+    Application,
+    Router,
+    h,
+    Helmet,
+    renderSSR
+} from './deps.ts'
+
+import {App} from './app.tsx'
+
+const ssr = renderSSR(<App />)
+const { body, head, footer } = Helmet.SSR(ssr)
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    ${head.join('\n')}
+  </head>
+  <body>
+    ${body}
+    ${footer.join('\n')}
+  </body>
+</html>`
+
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = html;
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+  "listen",
+  (e) => console.log("Listening on http://localhost:8080"),
+);
+await app.listen({ port: 8080 });
